88e935ea83bdfed562758be7840fcebbc1e98756
Frostlag <dingtaoyin@Hotmail.com>
2015-06-17 23:49:18 -0400

    Dialogmanager

--- app/src/main/java/com/example/dingtao/run/DialogManager.java
+++ app/src/main/java/com/example/dingtao/run/DialogManager.java
@@ -1,7 +1,24 @@
 package com.example.dingtao.run;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
 /**
  * Created by Dingtao on 6/17/2015.
  */
 public class DialogManager {
+    public static void Quit(final Context context){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage(R.string.dialog_new_eatList_title).setNegativeButton(R.string.dialog_cancel,null).setPositiveButton(R.string.dialog_confirm,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Model.get().newEatListConfirmed(((TextView)view.findViewById(R.id.dialog_new_eatList_content)).getText().toString());
+                    }
+                });
+        builder.setView(view);
+        AlertDialog dialog = builder.create();
+
+        dialog.show();
+    }
 }
